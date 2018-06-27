aecf4d071a0ef6bc31369ae23c96f855f15bc35c
Roman Krewer <roman.krewer@deutsche-boerse.com>
2018-06-27 21:02:08 +0200

    minimize settings

--- includes/class-kitaniasc-settings.php
+++ includes/class-kitaniasc-settings.php
@@ -112,105 +112,12 @@ class kitaniaSC_Settings {
 			'description'			=> __( 'These are fairly standard form input fields.', 'kitaniasc' ),
 			'fields'				=> array(
 				array(
-					'id' 			=> 'text_field',
-					'label'			=> __( 'Some Text' , 'kitaniasc' ),
-					'description'	=> __( 'This is a standard text field.', 'kitaniasc' ),
-					'type'			=> 'text',
-					'default'		=> '',
-					'placeholder'	=> __( 'Placeholder text', 'kitaniasc' )
-				),
-				array(
-					'id' 			=> 'password_field',
-					'label'			=> __( 'A Password' , 'kitaniasc' ),
-					'description'	=> __( 'This is a standard password field.', 'kitaniasc' ),
-					'type'			=> 'password',
-					'default'		=> '',
-					'placeholder'	=> __( 'Placeholder text', 'kitaniasc' )
-				),
-				array(
-					'id' 			=> 'secret_text_field',
-					'label'			=> __( 'Some Secret Text' , 'kitaniasc' ),
-					'description'	=> __( 'This is a secret text field - any data saved here will not be displayed after the page has reloaded, but it will be saved.', 'kitaniasc' ),
-					'type'			=> 'text_secret',
-					'default'		=> '',
-					'placeholder'	=> __( 'Placeholder text', 'kitaniasc' )
-				),
-				array(
 					'id' 			=> 'text_block',
-					'label'			=> __( 'A Text Block' , 'kitaniasc' ),
-					'description'	=> __( 'This is a standard text area.', 'kitaniasc' ),
+					'label'			=> __( 'Text block' , 'kitaniasc' ),
+					'description'	=> __( 'HTML code, der eingefuegt werden soll.', 'kitaniasc' ),
 					'type'			=> 'textarea',
 					'default'		=> '',
-					'placeholder'	=> __( 'Placeholder text for this textarea', 'kitaniasc' )
-				),
-				array(
-					'id' 			=> 'single_checkbox',
-					'label'			=> __( 'An Option', 'kitaniasc' ),
-					'description'	=> __( 'A standard checkbox - if you save this option as checked then it will store the option as \'on\', otherwise it will be an empty string.', 'kitaniasc' ),
-					'type'			=> 'checkbox',
-					'default'		=> ''
-				),
-				array(
-					'id' 			=> 'select_box',
-					'label'			=> __( 'A Select Box', 'kitaniasc' ),
-					'description'	=> __( 'A standard select box.', 'kitaniasc' ),
-					'type'			=> 'select',
-					'options'		=> array( 'drupal' => 'Drupal', 'joomla' => 'Joomla', 'wordpress' => 'WordPress' ),
-					'default'		=> 'wordpress'
-				),
-				array(
-					'id' 			=> 'radio_buttons',
-					'label'			=> __( 'Some Options', 'kitaniasc' ),
-					'description'	=> __( 'A standard set of radio buttons.', 'kitaniasc' ),
-					'type'			=> 'radio',
-					'options'		=> array( 'superman' => 'Superman', 'batman' => 'Batman', 'ironman' => 'Iron Man' ),
-					'default'		=> 'batman'
-				),
-				array(
-					'id' 			=> 'multiple_checkboxes',
-					'label'			=> __( 'Some Items', 'kitaniasc' ),
-					'description'	=> __( 'You can select multiple items and they will be stored as an array.', 'kitaniasc' ),
-					'type'			=> 'checkbox_multi',
-					'options'		=> array( 'square' => 'Square', 'circle' => 'Circle', 'rectangle' => 'Rectangle', 'triangle' => 'Triangle' ),
-					'default'		=> array( 'circle', 'triangle' )
-				)
-			)
-		);
-
-		$settings['extra'] = array(
-			'title'					=> __( 'Extra', 'kitaniasc' ),
-			'description'			=> __( 'These are some extra input fields that maybe aren\'t as common as the others.', 'kitaniasc' ),
-			'fields'				=> array(
-				array(
-					'id' 			=> 'number_field',
-					'label'			=> __( 'A Number' , 'kitaniasc' ),
-					'description'	=> __( 'This is a standard number field - if this field contains anything other than numbers then the form will not be submitted.', 'kitaniasc' ),
-					'type'			=> 'number',
-					'default'		=> '',
-					'placeholder'	=> __( '42', 'kitaniasc' )
-				),
-				array(
-					'id' 			=> 'colour_picker',
-					'label'			=> __( 'Pick a colour', 'kitaniasc' ),
-					'description'	=> __( 'This uses WordPress\' built-in colour picker - the option is stored as the colour\'s hex code.', 'kitaniasc' ),
-					'type'			=> 'color',
-					'default'		=> '#21759B'
-				),
-				array(
-					'id' 			=> 'an_image',
-					'label'			=> __( 'An Image' , 'kitaniasc' ),
-					'description'	=> __( 'This will upload an image to your media library and store the attachment ID in the option field. Once you have uploaded an imge the thumbnail will display above these buttons.', 'kitaniasc' ),
-					'type'			=> 'image',
-					'default'		=> '',
-					'placeholder'	=> ''
-				),
-				array(
-					'id' 			=> 'multi_select_box',
-					'label'			=> __( 'A Multi-Select Box', 'kitaniasc' ),
-					'description'	=> __( 'A standard multi-select box - the saved data is stored as an array.', 'kitaniasc' ),
-					'type'			=> 'select_multi',
-					'options'		=> array( 'linux' => 'Linux', 'mac' => 'Mac', 'windows' => 'Windows' ),
-					'default'		=> array( 'linux' )
+					'placeholder'	=> __( '<p>Beispielcode</p>', 'kitaniasc' )
 				)
 			)
 		);
