4029510233f49f9578eaa6b4316b1e64c06fb0de
Roman Krewer <roman.krewer@deutsche-boerse.com>
2018-06-27 21:13:53 +0200

    going deeper...

--- includes/class-kitaniasc.php
+++ includes/class-kitaniasc.php
@@ -119,7 +119,7 @@ class kitaniaSC {
 
 	public function render_shortcode()
 	{
-		return "<pre>".print_r($this->settings['standard'],1)."</pre>";
+		return "<pre>".print_r($this->settings->settings['standard'],1)."</pre>";
 	}
 
 	/**
